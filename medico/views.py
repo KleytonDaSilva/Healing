081ea633d61774b72f81c2aca6cd625caafdb2ef
kleyton da Silva <nksaddam@gmail.com>
2024-09-23 18:22:12 -0300

    atualizacao dia 23-09-2024

--- medico/views.py
+++ medico/views.py
@@ -30,7 +30,7 @@ def cadastro_medico(request):
         descricao = request.POST.get('descricao')
         valor_consulta  = request.POST.get('valor_consulta')
 
-        dados_medico = DadosMedico(
+        dados_medicos = DadosMedico(
             crm = crm,
             nome = nome,
             cep = cep,
@@ -46,7 +46,17 @@ def cadastro_medico(request):
             user=request.user
         )
 
-        dados_medico.save()
+        dados_medicos.save()
 
         messages.add_message(request, constants.SUCCESS, 'Cadastro médico realizado com sucesso')
         return redirect('/medicos/abrir_horario')
+    
+def abrir_horario(request):
+
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING,'Somente médico')
+        return redirect('/usuarios/sair')
+    
+    if request.method == "GET":
+        dados_medicos = DadosMedico.objects.get(user = request.user) 
+        return render (request, 'abrir_horario.html', {'dados_medicos' : dados_medicos})
